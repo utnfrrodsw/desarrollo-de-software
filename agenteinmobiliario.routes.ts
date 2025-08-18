117cbb1eb174460ec94cf76aeeae28df65fc1b00
palomatejedor <166176848+palomatejedor@users.noreply.github.com>
2025-08-18 18:30:55 -0300

    Update agenteinmobiliario.routes.ts

--- agenteinmobiliario.routes.ts
+++ agenteinmobiliario.routes.ts
@@ -1,17 +1,10 @@
 import { Router } from 'express';
-import {
-  sanitizeAgenteInput,
-  findAll,
-  findOne,
-  add,
-  update,
-  remove
-} from './agenteinmobiliario.controller.js';
+import { findAll, findOne, add, update, remove } from './agenteinmobiliario.controller.js';
 
 export const agenteinmobiliarioRouter = Router();
 
 agenteinmobiliarioRouter.get('/', findAll);
 agenteinmobiliarioRouter.get('/:id', findOne);
-agenteinmobiliarioRouter.post('/', sanitizeAgenteInput, add);
-agenteinmobiliarioRouter.put('/:id', sanitizeAgenteInput, update);
+agenteinmobiliarioRouter.post('/', add);
+agenteinmobiliarioRouter.put('/:id', update);
 agenteinmobiliarioRouter.delete('/:id', remove);
